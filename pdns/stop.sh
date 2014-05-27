2a80a5df5e508470fed8fea44b74f9fc9670d7aa
Nick Hildebrant <nick.hildebrant@gmail.com>
2014-05-27 14:04:39 +0000

    pdns working

--- pdns/stop.sh
+++ pdns/stop.sh
@@ -1,6 +1,2 @@
 docker stop pdns
 docker rm pdns
-docker stop pdns01
-docker rm pdns01
-docker stop pdns02
-docker rm pdns02
--- postgres/srv.sh
+++ postgres/srv.sh
@@ -1,1 +1,1 @@
-docker run -d --name=pg01 -p 5432:5432 -p 22 -v=/mnt/pg01/main:/pg/main:rw postgres
+docker run -d --name=pg01 -p 5432:5432 -p 22 -v=/data/pg01/main:/pg/main:rw postgres
